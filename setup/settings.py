79f9883acffa87c8c6131ea1fea2429114c988f8
unknown <marcostulio-dev>
2025-04-23 15:13:58 -0300

    Criação de rotas e configuração do código após formatação

--- setup/settings.py
+++ setup/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path, os
+from pathlib import Path
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'galeria',
 ]
 
 MIDDLEWARE = [
