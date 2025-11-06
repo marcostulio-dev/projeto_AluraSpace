cbb5824b27661aaa4f9c66d60a02d9d38e6539e7
marcostulio-dev <marcostuliomachadocosta@gmail.com>
2025-11-06 19:19:20 -0300

    Pequenos ajustes no BD e personalização do admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
-# Create your models here.
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = {
+        ("NEBULOSA", "Nebulosa"),
+        ("GALÁXIA", "Galáxia"),
+        ("ESTRELA", "Estrela"),
+        ("PLANETA", "Planeta"),
+    }
+
     nome = models.CharField(max_length = 100, null = False, blank = False)
     legenda = models.CharField(max_length = 150, null = False, blank = False)
+    categoria = models.CharField(max_length = 100, choices = OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null = False, blank = False)
     foto = models.CharField(max_length = 100, null = False, blank = False)
 
