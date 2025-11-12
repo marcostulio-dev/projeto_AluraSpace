60b172aad3bd2b41cd4beefa84823ee294ec0adb
marcostulio-dev <marcostuliomachadocosta@gmail.com>
2025-11-12 15:58:22 -0300

    Última commit

--- galeria/views.py
+++ galeria/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
+
 # Create your views here.
 
 def index(request):
 
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
 
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    return render(request, "galeria/buscar.html",)
